b7d02e5ac23a72dd86e3e2dd88ec79192301fcf6
Tony Campbell <tcampbell@github.com>
2017-08-15 09:26:58 -0700

    Add a data file for saving url redirects

--- redir.py
+++ redir.py
@@ -1,6 +1,22 @@
 import flask
-app = flask.Flask(__name__)
+import yaml
 
-@app.route('/')
-def index():
-    return flask.redirect('https://github.com', code=302)
+
+app = flask.Flask(__name__)
+url_map = yaml.load(open('urls.yml'))
+
+
+# reserve admin actions first
+@app.route('/reload')
+def reload():
+    # FIXME don't use a global
+    global url_map
+    url_map = yaml.load(open('urls.yml'))
+
+
+@app.route('/<string:short_link>')
+def index(short_link):
+    if short_link not in url_map:
+        return flask.abort(404)
+    # TODO log and monitor the redirection
+    return flask.redirect(url_map[short_link], code=302)
